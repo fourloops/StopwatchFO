5463d8fb041565aac5c07e72bacb69a761dbe9fe
Owen TM <oturnermajor@gmail.com>
2016-01-27 17:57:53 +0000

    working buttons!

--- stopwatch.js
+++ stopwatch.js
@@ -2,11 +2,15 @@ var T = {
         startTime   :  0,
         stopTime    :  0,                         // watch out when adding stop-go function
         running     : false,
-        elapsed     :  0
+        elapsed     :  0,
+        hours       :  0,
+        minutes     :  0,
+        seconds     :  0,
+        cSeconds    :  0
 };
 
 function caltime(){
-    return Math.floor(Date.now()/100);
+    return Math.floor(Date.now()/10);
 }
 
 function start(){
@@ -22,10 +26,32 @@ function stop(){
 function updateTime(){
     if(T.running === false){ T.elapsed = T.stopTime - T.startTime; }
     else { T.elapsed = caltime() - T.startTime; }
+    T.hours = Math.floor(T.elapsed/360000);
+    T.minutes = Math.floor((T.elapsed - T.hours*360000)/6000);
+    T.seconds = Math.floor((T.elapsed - T.minutes*6000 - T.hours*360000)/100);
+    T.cSeconds = (T.elapsed - T.seconds*100 - T.minutes*6000 - T.hours*360000);
 }
 function reset(){
     T.startTime  = 0;
     T.stopTime   = 0;
     T.running    = false;
     T.elapsed    = 0;
+    T.hours      = 0;
+    T.minutes    = 0;
+    T.seconds    = 0;
+    T.cSeconds   = 0;
 }
+
+document.getElementById('start').addEventListener('click', start);
+document.getElementById('stop').addEventListener('click', stop);
+document.getElementById('reset').addEventListener('click', reset);
+
+
+
+setInterval(function(){
+        updateTime();
+        document.getElementById('hours').innerHTML = T.hours ? T.hours : '';
+        document.getElementById('minutes').innerHTML = T.minutes ? T.minutes : '';
+        document.getElementById('seconds').innerHTML = T.seconds ? T.seconds : '';
+        document.getElementById('cSeconds').innerHTML = T.cSeconds ? T.cSeconds : '';
+}, 10 );
--- test/test.js
+++ test/test.js
@@ -16,19 +16,21 @@ test( "T.elapsed should increase after start() and updateTime()", function( asse
   setTimeout(function() {
       updateTime();
       assert.ok( T.elapsed > 0, "T.elapsed is greater than 0" );
-      console.log(T.elapsed);
       done1();
-  }, 2000 );
-
+  }, 100 );
 });
 
 test("reset should change T.running to false, and T.startTime and T.stopTime to 0", function(){
-    reset(T);
+    reset();
     T3 = {
-        startTime   :  0,
-        stopTime    :  0,                         // watch out when adding stop-go function
-        running     : false,
-        elapsed     :  0
+            startTime   :  0,
+            stopTime    :  0,                         // watch out when adding stop-go function
+            running     : false,
+            elapsed     :  0,
+            hours       :  0,
+            minutes     :  0,
+            seconds     :  0,
+            cSeconds    :  0
     };
     deepEqual(T, T3, "Reset sets all variables to original state");
 });
@@ -38,4 +40,28 @@ test("elapsed time should be accessed in terms of hours, minutes, seconds, centi
     ok(T.minutes    !== undefined,"T object should have minutes");
     ok(T.seconds    !== undefined,"T object should have seconds");
     ok(T.cSeconds   !== undefined,"T object should have centiseconds");
+});
+
+
+test( "T.elapsed should equal the timeOut", function( assert ) {
+    reset();
+    var done1 = assert.async();
+    start();
+    setTimeout(function() {
+        updateTime();
+        assert.equal( T.elapsed, 11, "T.elapsed is greater than 0" );
+        done1();
+    }, 1100 );
+});
+
+test( "Hours,  minutes, seconds, cSeconds should change with updateTime()", function( assert ) {
+    reset();
+    start();
+    T.startTime = caltime() - 366107;
+    updateTime();
+    assert.equal( T.hours, 1, "T.hours is correct" );
+    assert.equal( T.minutes, 1, "T.minutes is correct" );
+    assert.equal( T.seconds, 1, "T.seconds is correct" );
+    assert.equal( T.cSeconds, 7, "T.cSeconds is correct" );
+    reset();
 });
