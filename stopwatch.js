3a9adb764e30c02a87c8b02817320199ad5dcd42
franzmoro <franzmoro@hotmail.com>
2016-01-27 12:54:56 +0000

    added display time return object and test for calendar time

--- stopwatch.js
+++ stopwatch.js
@@ -2,8 +2,18 @@ var T = {
     timeElapsed: 0
 };
 function displayTime(){
-    var hours = document.getElementById('hours');
-    var minutes = document.getElementById('minutes');
-    var seconds = document.getElementById('seconds');
-    var cSeconds = document.getElementById('cSeconds');
+    var hours = document.getElementById('hours'),
+        minutes = document.getElementById('minutes'),
+        seconds = document.getElementById('seconds'),
+        cSeconds = document.getElementById('cSeconds');
+    /*hours.innerHTML = 00;
+    minutes.innerHTML = 00;
+    seconds.innerHTML = 00;
+    cSeconds.innerHTML = 00;*/
+    return {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        cSeconds: 0
+    }
 }
--- test/test.js
+++ test/test.js
@@ -3,5 +3,8 @@ test( "timeElapsed should be Zero before we start the Timer", function() {
 });
 
 test('displayTime function should return contents of all stopwatch spans', function() {
-    equal(displayTime(), {hours: 0, minutes: 0, seconds: 0, cSeconds: 0}, 'Message: displayTime returns correct values');
+    deepEqual(displayTime(), {hours: 0, minutes: 0, seconds: 0, cSeconds: 0}, 'Message: displayTime returns correct values');
+});
+test("T.caltime should return the current calendare time in centiseconds",function(){
+    equal(T.caltime,Math.floor( Date.now() / 100 ),"Calendar time in cs should be returned");
 });
